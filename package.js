88a0ba225e941d0be185862df4b3f3057b84cd14
Ronen Babayoff <ronen@lavaina.com>
2014-09-09 00:15:49 -0400

    Semantics only

--- package.js
+++ package.js
@@ -4,20 +4,21 @@ Package.describe({
 
 Package.on_use(function (api, where) {
     api.use(["coffeescript","tinytest","test-helpers","chai","sinon","underscore"]);
-    api.export(['lvTestAsyncMulti']);
-    api.export(['Munit', 'chai']);
-    api.export(['describe', 'it', 'beforeAll', 'beforeEach', 'afterEach', 'afterAll']);
 
     api.add_files("namespaces.js");
     api.add_files("async_multi.js");
     api.add_files("Munit.coffee");
     api.add_files("Helpers.coffee");
     api.add_files("Describe.coffee");
+
+  api.export(['lvTestAsyncMulti']);
+  api.export(['Munit', 'chai']);
+  api.export(['describe', 'it', 'beforeAll', 'beforeEach', 'afterEach', 'afterAll']);
 });
 
 Package.on_test(function(api) {
     api.use(["coffeescript" ,"tinytest" ,"test-helpers", "munit"]);
-    api.add_files("tests/TestRunnerTest.coffee")
-    api.add_files("tests/HelpersTest.coffee")
-    api.add_files("tests/DescribeTest.coffee")
+    api.add_files("tests/TestRunnerTest.coffee");
+    api.add_files("tests/HelpersTest.coffee");
+    api.add_files("tests/DescribeTest.coffee");
 });
