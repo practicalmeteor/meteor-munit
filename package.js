49111dcb9045f75573c6caa7f60b9105a4957bfb
Juan Sepulveda <juan@lavaina.com>
2014-09-19 18:00:56 -0400

    Bump version

--- package.js
+++ package.js
@@ -1,29 +1,30 @@
 Package.describe({
     summary: "Meteor unit testing framework for packages",
     name: "spacejamio:munit",
-    version: "0.2.1",
+    version: "1.0.0",
     git: "https://github.com/spacejamio/meteor-munit.git"
 });
 
-Package.on_use(function (api, where) {
-  api.use(["coffeescript","tinytest","test-helpers","underscore"]);
+Package.onUse(function (api, where) {
+  api.use(["coffeescript", "underscore"]);
 
-  api.use(["spacejamio:chai@0.1.6","spacejamio:sinon@0.1.6"]);
+  api.imply(["tinytest","test-helpers"]);
+  api.imply(["spacejamio:chai","spacejamio:sinon"]);
 
-  api.add_files("namespaces.js");
-  api.add_files("async_multi.js");
-  api.add_files("Munit.coffee");
-  api.add_files("Helpers.coffee");
-  api.add_files("Describe.coffee");
+  api.addFiles("namespaces.js");
+  api.addFiles("async_multi.js");
+  api.addFiles("Munit.coffee");
+  api.addFiles("Helpers.coffee");
+  api.addFiles("Describe.coffee");
 
   api.export(['lvTestAsyncMulti']);
   api.export(['Munit', 'chai']);
   api.export(['describe', 'it', 'beforeAll', 'beforeEach', 'afterEach', 'afterAll']);
 });
 
-Package.on_test(function(api) {
-    api.use(["coffeescript" ,"tinytest" ,"test-helpers", "spacejamio:munit"]);
-    api.add_files("tests/TestRunnerTest.coffee");
-    api.add_files("tests/HelpersTest.coffee");
-    api.add_files("tests/DescribeTest.coffee");
+Package.onTest(function(api) {
+    api.use(["coffeescript", "spacejamio:munit"]);
+    api.addFiles("tests/TestRunnerTest.coffee");
+    api.addFiles("tests/HelpersTest.coffee");
+    api.addFiles("tests/DescribeTest.coffee");
 });
