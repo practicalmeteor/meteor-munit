126c272174d7e3fd6bd17cb07ca3cd7f228578b6
Ronen Babayoff <ronen@lavaina.com>
2014-10-10 22:22:30 -0400

    Bump package version to 2.0.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     summary: "Meteor unit testing framework for packages",
     name: "spacejamio:munit",
-    version: "1.0.0",
+    version: "2.0.0",
     git: "https://github.com/spacejamio/meteor-munit.git"
 });
 
